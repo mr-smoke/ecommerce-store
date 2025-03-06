9c742567f2c4804602aae480d6882367b286b0fa
baki duman <g.s_baki_70@hotmail.com>
2025-03-06 10:55:25 +0300

    Add MongoDB connection and dotenv support; update .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 const app = express();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
