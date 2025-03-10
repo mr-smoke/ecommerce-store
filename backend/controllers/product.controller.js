6adae81c5e44d52d9034de35a438185267f2ae58
baki duman <g.s_baki_70@hotmail.com>
2025-03-10 12:37:14 +0300

    Add product retrieval endpoints for all products, featured products, and products by category

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import { redis } from "../lib/redis.js";
 
 export const createProduct = async (req, res) => {
   try {
@@ -22,6 +23,48 @@ export const deleteProduct = async (req, res) => {
     await Product.findByIdAndDelete(req.params.id);
 
     res.status(201).json({ product });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({ products });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let products = await redis.get("featuredProducts");
+
+    if (products) {
+      return res.status(200).json({ products: JSON.parse(products) });
+    }
+
+    products = await Product.find({ featured: true });
+
+    if (!products) {
+      return res.status(404).json({ error: "No featured products found" });
+    }
+
+    await redis.set("featuredProducts", JSON.stringify(products));
+
+    res.status(200).json({ products });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getProductByCategory = async (req, res) => {
+  try {
+    const products = await Product.find({ category: req.params.category });
+
+    res.status(200).json({ products });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,11 +2,17 @@ import express from "express";
 import {
   createProduct,
   deleteProduct,
+  getProducts,
+  getFeaturedProducts,
+  getProductByCategory,
 } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createProduct);
 router.delete("/delete/:id", deleteProduct);
+router.get("/", getProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductByCategory);
 
 export default router;
