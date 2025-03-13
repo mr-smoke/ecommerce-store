f202da78bedbe3ee10b756fc3629ea6cdf7b54ea
baki duman <g.s_baki_70@hotmail.com>
2025-03-13 10:53:50 +0300

    Add getCartItems endpoint to retrieve items in the user's cart

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -11,7 +11,7 @@ export const addToCart = async (req, res) => {
     if (isExisting) {
       isExisting.quantity += 1;
     } else {
-      user.cartItems.push({ id: productId, quantity: 1 });
+      user.cartItems.push(productId);
     }
 
     await user.save();
@@ -39,6 +39,27 @@ export const removeFromCart = async (req, res) => {
     res.status(201).json(user.cartItems);
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getCartItems = async (req, res) => {
+  try {
+    const user = req.user;
+
+    const products = await Product.find({
+      _id: { $in: user.cartItems },
+    });
+
+    const cartItems = products.map((product) => {
+      const item = req.user.cartItems.find(
+        (cartItem) => cartItem.id === product.id
+      );
+      return { ...product.toJSON(), quantity: item.quantity };
+    });
+
+    res.status(200).json(cartItems);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { addToCart, removeFromCart } from "../controllers/cart.controller.js";
+import {
+  addToCart,
+  removeFromCart,
+  getCartItems,
+} from "../controllers/cart.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/add/:id", protectRoute, addToCart);
 router.delete("/remove", protectRoute, removeFromCart);
+router.get("/", protectRoute, getCartItems);
 
 export default router;
