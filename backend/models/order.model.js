7522aef0d75a866b0cf104363373d6322de711a8
baki duman <g.s_baki_70@hotmail.com>
2025-03-14 15:57:21 +0300

    Add Order model to manage user orders and associated items

---
+++ backend/models/order.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    stripeSessionId: {
+      type: String,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
